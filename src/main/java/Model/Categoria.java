bfa5c9cdca4aa23a74c4efa5b0599115d5126001
Tiago-PC <Tiago-PC@DESKTOP-AKQH14S>
2018-11-26 21:53:48 -0200

    Commit Fornecedor Tiago/Everton

--- src/main/java/Model/Categoria.java
+++ src/main/java/Model/Categoria.java
@@ -5,59 +5,12 @@
  */
 package Model;
 
-import java.io.Serializable;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-
 /**
  *
- * @author aluno
+ * @author Tiago-PC
  */
-@Entity
-public class Categoria implements Serializable {
-    private static final long serialVersionUID = 1L;
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private String roupa;
-    private String calcado;
-    private String acessorio;
+public enum Categoria {
     
-    private Long id;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    
-    @Override
-    public int hashCode() {
-        int hash = 0;
-        hash += (id != null ? id.hashCode() : 0);
-        return hash;
-    }
-
-    @Override
-    public boolean equals(Object object) {
-        // TODO: Warning - this method won't work in the case the id fields are not set
-        if (!(object instanceof Categoria)) {
-            return false;
-        }
-        Categoria other = (Categoria) object;
-        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
-            return false;
-        }
-        return true;
-    }
-
-    @Override
-    public String toString() {
-        return "modelV1.Categoria[ id=" + id + " ]";
-    }
+    calcado,roupa,acessorio;
     
 }
